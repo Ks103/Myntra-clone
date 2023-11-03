e54c23edccc3f5abf8976fc934b96c4ddf7e534c
Khushi Shroff <kkhushi3058@gmail.com>
2023-11-03 16:25:28 +0530

    #1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { store } from "./ReduxStore/store"
+import { Provider } from "react-redux";
+import { HashRouter } from 'react-router-dom'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+
+  document.getElementById("root")
+);
